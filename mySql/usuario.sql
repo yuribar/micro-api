78930352284fb1002443c2bae4d72887e22f93a2
MasterRacer - Yuri <yuribarbosa123@hotmail.com>
2022-04-29 19:23:48 -0300

    mySql changes

---
+++ mySql/usuario.sql
@@ -0,0 +1,4 @@
+create database if not exists microservice;
+use microservice;
+create table if not exists usuario (id INTEGER PRIMARY KEY, nome VARCHAR(50) NOT NULL, email VARCHAR(100));
+insert into usuario values (1234, "Yuri Barbosa", "yuribarbosa@hotmail.com");
