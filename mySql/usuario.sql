e1a700b8bf7c7c340be435a4189bf7a9de537792
MasterRacer - Yuri <yuribarbosa123@hotmail.com>
2022-04-30 10:36:39 -0300

    teste9

--- mySql/usuario.sql
+++ mySql/usuario.sql
@@ -2,4 +2,5 @@ create database if not exists microservice;
 use microservice;
 create table if not exists usuario (id INTEGER PRIMARY KEY, nome VARCHAR(50) NOT NULL, email VARCHAR(100));
 
-INSERT INTO usuario (id, nome, email ) VALUES (1234, "Yuri", "yuribarbosa@hotmail.com", 5678, "Mayara", "mayara.suzanaa@gmail.com");
+INSERT INTO usuario (id, nome, email ) VALUES (1234, "Yuri", "yuribarbosa@hotmail.com");
+INSERT INTO usuario (id, nome, email ) VALUES (5678, "Mayara", "mayara.suzanaa@gmail.com");
