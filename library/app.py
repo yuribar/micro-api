793ca059c8c8275a6e419f5a982c19ab9637866f
MasterRacer - Yuri <yuribarbosa123@hotmail.com>
2022-04-30 09:51:51 -0300

    test1

--- library/app.py
+++ library/app.py
@@ -1,25 +1,25 @@
 #!/usr/bin/env python
 
 from flask import Flask, Response, request
-#from flask_sqlalchemy import SQLAlchemy
-#import mysql.connector
+from flask_sqlalchemy import SQLAlchemy
+import mysql.connector
 import json
 
 
 
 app = Flask(__name__)
-#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Qwert78940*@localhost/microservice'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Qwert78940*@localhost/microservice'
 
-#db = SQLAlchemy(app)
+db = SQLAlchemy(app)
 
-#class Usuario(db.Model):
-#    id = db.Column(db.Integer, primary_key= True)
-#    nome = db.Column(db.String(50))
-#    email = db.Column(db.String(100))
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key= True)
+    nome = db.Column(db.String(50))
+    email = db.Column(db.String(100))
     
-#    def to_json(self):
-#       return {"id": self.id, "nome": self.nome, "email": self.email}
+    def to_json(self):
+       return {"id": self.id, "nome": self.nome, "email": self.email}
 
 @app.route("/")
 def hello():
